053f509b0bd91888c7677e14c2bd237c86d306e1
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-13 13:15:00 +0530

    created task-dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskDashboardComponent } from './task-dashboard/task-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskDashboardComponent
   ],
   imports: [
     BrowserModule
